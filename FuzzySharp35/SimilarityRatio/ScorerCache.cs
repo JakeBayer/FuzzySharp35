d1ac8cdfc96ddee70ffeea6a91aa4e84b8da1a40
Jake Bayer <jacob.bayer@blackstone.com>
2019-05-22 14:06:00 -0400

    .net 3.5 port of FuzzySharp

---
+++ FuzzySharp35/SimilarityRatio/ScorerCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using FuzzySharp.SimilarityRatio.Scorer;
+
+namespace FuzzySharp.SimilarityRatio
+{
+    public static class ScorerCache
+    {
+        private static readonly Dictionary<Type, IRatioScorer> s_scorerCache = new Dictionary<Type, IRatioScorer>();
+        private static readonly object _mutex = new object();
+        public static IRatioScorer Get<T>() where T : IRatioScorer, new()
+        {
+            lock (_mutex)
+            {
+                var typeOfT = typeof(T);
+                if (!s_scorerCache.ContainsKey(typeOfT))
+                {
+                    s_scorerCache.Add(typeOfT, new T());
+                }
+                return s_scorerCache[typeOfT];
+            }
+            
+        }
+    }
+}
---
+++ FuzzySharp35/SimilarityRatio/Strategy/DefaultRatioStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuzzySharp.SimilarityRatio.Strategy
+{
+    internal class DefaultRatioStrategy
+    {
+        public static int Calculate(string input1, string input2)
+        {
+            return (int)Math.Round(100 * Levenshtein.GetRatio(input1, input2));
+        }
+    }
+}
